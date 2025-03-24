5691fc4974333cfc380e8391a48ff326e8172509
TobyMunyard <tobymunyard@gmail.com>
2025-03-24 16:19:00 +1300

    Added Input Validation For Account Creation
    Added checks for each user input when creating an account to ensure they satisfy things like length and not null requirements. Added custom error messages to be shown on the front-end. References #1, references #5.

--- src/main/java/com/example/flattie/controller/CreateAccountController.java
+++ src/main/java/com/example/flattie/controller/CreateAccountController.java
@@ -3,8 +3,10 @@ package com.example.flattie.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.flattie.model.AppUser;
 import com.example.flattie.service.AppUserService;
@@ -35,15 +37,45 @@ public class CreateAccountController {
     @PostMapping("/createAccount")
     public String createAccount(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName,
             @RequestParam("username") String username, @RequestParam("password") String password,
-            @RequestParam("confirmPassword") String confirmPassword) {
+            @RequestParam("confirmPassword") String confirmPassword, Model model,
+            RedirectAttributes redirectAttributes) {
 
-        if (password.equals(confirmPassword)) {
-            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-            AppUser newUser = new AppUser(firstName, lastName, username, passwordEncoder.encode(password));
-            System.out.println("Working: " + newUser);
-            appUserService.saveAppUser(newUser);
+        // In case no errors occur this will satisy thymeleaf
+        model.addAttribute("error", null);
+
+        // Validation done manually because annotations only work for entire entity and
+        // manual error messages are nicer for users
+        
+        if (firstName == null || firstName.isBlank() || firstName.length() > 20) {
+            redirectAttributes.addFlashAttribute("error", "First name must be under 20 characters.");
+            return "redirect:/createAccount";
         }
 
-        return "login";
+        if (lastName == null || lastName.isBlank() || lastName.length() > 40) {
+            redirectAttributes.addFlashAttribute("error", "Last name must be under 40 characters.");
+            return "redirect:/createAccount";
+        }
+
+        if (username == null || username.isBlank() || username.length() < 5 || username.length() > 20) {
+            redirectAttributes.addFlashAttribute("error", "Username must be between 5 and 20 characters.");
+            return "redirect:/createAccount";
+        }
+
+        if (password == null || password.isBlank() || password.length() < 7 || password.length() > 50) {
+            redirectAttributes.addFlashAttribute("error", "Password must be at least 7 characters long.");
+            return "redirect:/createAccount";
+        }
+
+        if (!password.equals(confirmPassword)) {
+            redirectAttributes.addFlashAttribute("error", "Passwords do not match.");
+            return "redirect:/createAccount";
+        }
+
+        // User inputs are all acceptable, create account
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        AppUser newUser = new AppUser(firstName, lastName, username, passwordEncoder.encode(password));
+        appUserService.saveAppUser(newUser);
+
+        return "redirect:/login";
     }
 }
