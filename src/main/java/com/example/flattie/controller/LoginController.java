347355b078b70a02b974c77fb6c6977928c421d3
TobyMunyard <tobymunyard@gmail.com>
2025-03-24 14:27:08 +1300

    Added Error Message For Incorrect User Details
    Added a generic error message without too much info to be displayed if a username and password do not match an existing entry in the database. Used flashAttributes instead of model.addAttribute because these will only be there for the one redirect and will be removed once successfully logged in.

--- src/main/java/com/example/flattie/controller/LoginController.java
+++ src/main/java/com/example/flattie/controller/LoginController.java
@@ -3,8 +3,10 @@ package com.example.flattie.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.flattie.model.AppUser;
 import com.example.flattie.service.AppUserService;
@@ -16,21 +18,31 @@ public class LoginController {
     AppUserService appUserService;
 
     @PostMapping("/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password,
+            Model model, RedirectAttributes redirectAttributes) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        boolean usernameExists = true;
+        boolean passwordsMatch = true;
 
         AppUser existingUser = appUserService.getAppUserByUsername(username).orElse(null);
         if (existingUser == null) {
             // Username does not exist, return login page again
-            return "login";
-        }
-        if (passwordEncoder.encode(password).equals((existingUser.getPassword()))) {
-            System.out.println("Working: " + existingUser);
-            return "joinFlat";
-        } else {
-            // Password does not match saved one
-            return "login";
+            usernameExists = false;
+            redirectAttributes.addFlashAttribute("usernameExists", usernameExists);
+            return "redirect:/login";
         }
 
+        if (!passwordEncoder.matches(password, existingUser.getPassword())) {
+            // Password does not match saved one
+            passwordsMatch = false;
+            redirectAttributes.addFlashAttribute("passwordsMatch", passwordsMatch);
+            return "redirect:/login";
+        }
+
+        // User exists and login correct
+        System.out.println("Working: " + existingUser);
+        model.addAttribute("user", existingUser);
+        return "redirect:/joinFlat";
     }
 }
