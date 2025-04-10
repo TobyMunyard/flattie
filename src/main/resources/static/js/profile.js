2444c8cf9d6463754ce0a91a6f790fb4a5bb0b33
TobyMunyard <tobymunyard@gmail.com>
2025-04-10 13:57:54 +1200

    Connected JavaScript Function To Editing Account
    Connected a listener to the edit account button for displaying the modal of account information. Added user information to profile page. References #29.

---
+++ src/main/resources/static/js/profile.js
@@ -0,0 +1,7 @@
+let editButton = document.getElementById('edit-account-button');
+
+editButton.addEventListener("click", toggleProfileModal);
+
+function toggleProfileModal(){
+    console.log("This function is working!");
+}
