f70cbfe8d9e64a20124ee369887942a329cb98ce
taybr713 <taybr713@student.otago.ac.nz>
2025-04-15 15:03:54 +1200

    Emailing Test:
    - added a test method for sending an email from the email address I plan to use for this sending to my personal email.
    - successfully sent an email from the web application! This was a lot more straightforward than I thought it would be
    
    closes #45

---
+++ src/main/java/com/example/flattie/controller/EmailController.java
@@ -0,0 +1,24 @@
+package com.example.flattie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.flattie.service.EmailService;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("/test-email")
+    public String testEmail() {
+        emailService.sendEmail(
+                "brooklynigtaylor@gmail.com",
+                "Test from Flattie",
+                "This is a test email from your system.");
+        return "Email attempted to send.";
+    }
+}
